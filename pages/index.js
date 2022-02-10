8c22646c85d8baae3d58eb3fa4a5196fc8f0bf78
seanstory-dev <seanstory.dev@gmail.com>
2022-02-10 17:34:59 +0900

    Add styling system

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import { Global, css } from "@emotion/react";
 import styled from "@emotion/styled";
 
-const globalStyle = css`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-`;
+// const globalStyle = css`
+//   * {
+//     box-sizing: border-box;
+//     margin: 0;
+//     padding: 0;
+//   }
+// `;
 
-const Title = styled.h1`
-  color: red;
-`;
+// const Title = styled.h1`
+//   color: red;
+// `;
 
 const Home = () => {
   return (
     <>
-      <Global styles={globalStyle} />
-      <Title>index</Title>
+      {/* <Global styles={globalStyle} /> */}
+      <h1>Home</h1>
     </>
   );
 };
