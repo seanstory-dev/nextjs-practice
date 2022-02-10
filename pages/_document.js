adfec1103e8f39373eca146084a9696e6fcf588a
seanstory-dev <seanstory.dev@gmail.com>
2022-02-10 18:20:21 +0900

    Add assets - logo, favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,29 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        {/* favicons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#c4b9e1" />
+        <meta name="msapplication-TileColor" content="#c4b9e1" />
+        <meta name="theme-color" content="#c4b9e1" />
+        {/* fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
