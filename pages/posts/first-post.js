3cf766caf06802fb20c3b302aba3f2d273b2d04f
seanstory-dev <seanstory.dev@gmail.com>
2022-02-11 00:05:49 +0900

    Install sass and css styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,9 +2,11 @@ import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
 
+import Layout from "../../components/layout";
+
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
         {/* <script src="https://connect.facebook.net/en_US/sdk.js" /> */}
@@ -22,7 +24,7 @@ const FirstPost = () => {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 };
 
