eece1df4ea69936c753fdd6fd0162342528224d6
seanstory-dev <seanstory.dev@gmail.com>
2022-02-10 23:25:29 +0900

    Add pages and link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,13 @@
+import Link from "next/link"; /* a 태그는 새로고침 발생 */
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <h1 className="title">
+      Read{" "}
+      <Link href="/posts/first-post">
+        <a>this page</a>
+      </Link>
+    </h1>
+  );
 };
 export default Home;
---
+++ pages/posts/first-post.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const FirstPost = () => {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+};
+
+export default FirstPost;
+
+// http://localhost:3000/posts/first-post
